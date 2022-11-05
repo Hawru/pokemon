64ed1e952094a10be06f704117bb0ed56f023365
Hawru <imharu01@gmail.com>
2022-11-05 16:43:06 -0500

    arreglos menores

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,7 +3,6 @@ const Lista = require("../database/pokemon.json")
 
 const mainController = {
     index: (req, res) => {
-        console.log(Lista[1].base["Sp. Attack"])
         res.render("index.ejs", {pokemon: Lista})
     }
 }
